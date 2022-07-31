11b72832d764dacb0c3340737f4233664dcefc9c
vados <vados@vadosware.io>
2022-07-31 21:09:48 +0900

    fix: test setup
    Signed-off-by: vados <vados@vadosware.io>

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,15 @@ mod uuid_v6;
 use pgx::*;
 
 pg_module_magic!();
+
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
+}

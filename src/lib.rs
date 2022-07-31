522d173089ebca9af1708d4941a6f7e6f7c61638
vados <vados@vadosware.io>
2022-07-31 20:46:31 +0900

    fix: remove unimplemented modules
    Signed-off-by: vados <vados@vadosware.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 mod uuid_v6;
-mod uuid_v7;
-mod nanoid;
-mod ulid;
-mod ksuid;
-mod twtr_snowflake;
-mod timeflake;
-mod sonyflake;
-mod pushid;
-mod xid;
-mod cuid;
+// mod uuid_v7;
+// mod nanoid;
+// mod ulid;
+// mod ksuid;
+// mod twtr_snowflake;
+// mod timeflake;
+// mod sonyflake;
+// mod pushid;
+// mod xid;
+// mod cuid;
 
 use pgx::*;
 

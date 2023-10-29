7ff17e4827f4bd367dee24f24d03d27bc5a703ae
vados <vados@vadosware.io>
2023-10-29 14:45:07 +0900

    feat(cuid): add timestamp extraction fn for cuid
    Some ID generation functions have timestamps embedded in them, and
    being able to extract the timestamp is a useful feature. Timestamps
    extracted from IDs can be used in logic checks, as
    virtual/generated columns and have many other use cases.
    
    This commit adds functions for extracting timestamps from CUID
    values (even though they are deprecated in favor of CUID2).
    
    Signed-off-by: vados <vados@vadosware.io>

--- src/cuid.rs
+++ src/cuid.rs
@@ -1,9 +1,19 @@
+use std::io;
+
+use chrono::{Datelike, NaiveDateTime, Timelike};
 use cuid;
 use pgrx::*;
+
+use crate::common::OrPgxError;
 
 /// Generate a random cuid UUID
 #[pg_extern]
 fn idkit_cuid_generate() -> String {
+    // Ignore the deprecated version here in case people are using cuid
+    warning!(
+        "cuid is deprecated in favor of cuid2, consider using cuid2 (also available in pg_idkit)"
+    );
+    #[allow(deprecated)]
     match cuid::cuid() {
         Err(e) => error!("failed to generate cuid: {}", e),
         Ok(id) => id,
@@ -16,6 +26,56 @@ fn idkit_cuid_generate_text() -> String {
     idkit_cuid_generate()
 }
 
+/// Retrieve a `timestamptz` from a given textual CUID
+///
+/// If the value is not a valid CUID, this function will throw an error
+#[pg_extern]
+fn idkit_cuid_extract_timestamptz_utc(val: String) -> pgrx::TimestampWithTimeZone {
+    #[allow(deprecated)]
+    if !cuid::is_cuid(&val) {
+        pgrx::error!("value provided is not a valid CUID");
+    }
+
+    // Get the base64 epoch milliseconds
+    let epoch_millis = base36::decode(&val[1..9])
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{e:?}")))
+        .or_pgrx_error("failed to base64 decode cuid timestamp");
+    let epoch_millis_len = epoch_millis.len();
+
+    if epoch_millis_len > 8 {
+        pgrx::error!("unexpected length of bytes [{}]", epoch_millis.len());
+    };
+
+    // Copy in bytes from epoch conversion, copy to i64
+    let mut be_bytes = [0; 8];
+    be_bytes[8 - epoch_millis_len..].copy_from_slice(&epoch_millis);
+    let millis_i64 = i64::from_be_bytes(be_bytes);
+
+    // Convert to a UTC timestamp
+    let now = NaiveDateTime::from_timestamp_millis(millis_i64)
+        .or_pgrx_error("failed to parse timestamp from millis")
+        .and_utc();
+
+    pgrx::TimestampWithTimeZone::with_timezone(
+        now.year(),
+        now.month()
+            .try_into()
+            .or_pgrx_error("failed to convert months"),
+        now.day().try_into().or_pgrx_error("failed to convert days"),
+        now.hour()
+            .try_into()
+            .or_pgrx_error("failed to convert hours"),
+        now.minute()
+            .try_into()
+            .or_pgrx_error("failed to convert minutes"),
+        now.second()
+            .try_into()
+            .or_pgrx_error("failed to convert seconds"),
+        "utc",
+    )
+    .or_pgrx_error("failed to convert timestamp")
+}
+
 //////////
 // Test //
 //////////
@@ -23,12 +83,29 @@ fn idkit_cuid_generate_text() -> String {
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
 mod tests {
+    use chrono::{DateTime, Utc};
     use pgrx::*;
+
+    use crate::cuid::{idkit_cuid_extract_timestamptz_utc, idkit_cuid_generate};
 
     /// Basic length test
     #[pg_test]
     fn test_cuid_len() {
-        let generated = crate::cuid::idkit_cuid_generate();
+        let generated = idkit_cuid_generate();
         assert_eq!(generated.len(), 25);
+    }
+
+    /// Basic test of timestamp instantly generated and verified
+    #[pg_test]
+    fn test_cuid_extract_timestamp_utc() {
+        let cuid = idkit_cuid_generate();
+        let timestamp = idkit_cuid_extract_timestamptz_utc(cuid.clone());
+        let parsed: DateTime<Utc> = DateTime::parse_from_rfc3339(&timestamp.to_iso_string())
+            .expect("extracted timestamp as ISO string parsed to UTC DateTime")
+            .into();
+        assert!(
+            Utc::now().signed_duration_since(parsed).num_seconds() < 3,
+            "extracted, printed & re-parsed cuid timestamp is from recent past (within 3s)"
+        );
     }
 }

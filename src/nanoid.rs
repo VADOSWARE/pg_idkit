a30307e9fb5ddcfebd4b81b167c2f6e41d32241c
vados <vados@vadosware.io>
2024-01-29 16:34:02 +0900

    feat(nanoid): add customizable nanoid generation
    This commit adds customizable nanoid generation, including length and
    custom alphabets that can be passed to `nanoid`.

--- src/nanoid.rs
+++ src/nanoid.rs
@@ -1,3 +1,5 @@
+use std::convert::TryFrom;
+
 use nanoid::nanoid;
 use pgrx::*;
 
@@ -11,6 +13,19 @@ fn idkit_nanoid_generate() -> String {
 #[pg_extern]
 fn idkit_nanoid_generate_text() -> String {
     idkit_nanoid_generate()
+}
+
+/// Generate a nanoid, using a configurable alphabet, producing a Postgres text object
+#[pg_extern]
+fn idkit_nanoid_custom_generate_text(len: i64, alphabet: String) -> String {
+    let len = match usize::try_from(std::cmp::max(len, 0)) {
+        Ok(v) => v,
+        Err(e) => {
+            pgrx::error!("invalid length, cannot convert to platform-specific quantity: {e}")
+        }
+    };
+    let alphabet = alphabet.chars().collect::<Vec<char>>();
+    nanoid!(len, &alphabet)
 }
 
 //////////
@@ -27,5 +42,21 @@ mod tests {
     fn test_nanoid_len() {
         let generated = crate::nanoid::idkit_nanoid_generate();
         assert_eq!(generated.len(), 21);
+    }
+
+    #[pg_test]
+    /// Test a custom alphabet
+    fn test_nanoid_custom_alphabet() {
+        let generated = crate::nanoid::idkit_nanoid_custom_generate_text(21, "abc".into());
+        assert_eq!(generated.len(), 21);
+        assert!(generated.chars().all(|c| ('a'..='c').contains(&c)));
+    }
+
+    #[pg_test]
+    /// Test a custom len
+    fn test_nanoid_custom_len() {
+        let generated = crate::nanoid::idkit_nanoid_custom_generate_text(10, "abc".into());
+        assert_eq!(generated.len(), 10);
+        assert!(generated.chars().all(|c| ('a'..='c').contains(&c)));
     }
 }
